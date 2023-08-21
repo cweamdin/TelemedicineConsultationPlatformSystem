1599b5dcecfd4a8e6e1080b664349090039c24b2
jxc <jxc@qq.com>
2023-08-21 17:20:46 +0800

    区域管理类创建

---
+++ tcps-modules/tcps-system/src/main/java/com/tcps/system/service/impl/ISysAreaServiceImpl.java
@@ -0,0 +1,13 @@
+package com.tcps.system.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tcps.system.domain.SysArea;
+import com.tcps.system.mapper.SysAreaMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * * 区域管理接口实现类
+ */
+@Service
+public class ISysAreaServiceImpl extends ServiceImpl<SysAreaMapper, SysArea> {
+}

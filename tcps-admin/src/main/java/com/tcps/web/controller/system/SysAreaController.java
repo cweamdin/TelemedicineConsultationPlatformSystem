2d387b9d69e128e4d405e2dfeceec8f86bf716bc
jxc <jxc@qq.com>
2023-08-22 10:12:49 +0800

    区域管理类创建

---
+++ tcps-admin/src/main/java/com/tcps/web/controller/system/SysAreaController.java
@@ -0,0 +1,32 @@
+package com.tcps.web.controller.system;
+
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import com.tcps.common.core.controller.BaseController;
+import com.tcps.common.core.domain.R;
+import com.tcps.system.service.ISysAreaService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+/**
+ * * 系统区域控制层
+ */
+@Validated
+@RestController
+@RequestMapping("/system/sysArea")
+public class SysAreaController extends BaseController {
+
+    private ISysAreaService service;
+
+    /**
+     * * 查询区域所有信息
+     * @return
+     */
+    @SaCheckPermission("manage:area:list")
+    @GetMapping("/")
+    public R<List> findAllSysArea(){
+        return R.ok(service.list());
+    }
+}
--- tcps-modules/tcps-system/src/main/java/com/tcps/system/service/impl/ISysAreaServiceImpl.java
+++ tcps-modules/tcps-system/src/main/java/com/tcps/system/service/impl/ISysAreaServiceImpl.java
@@ -1,13 +1,17 @@
 package com.tcps.system.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tcps.system.domain.SysArea;
 import com.tcps.system.mapper.SysAreaMapper;
+import com.tcps.system.service.ISysAreaService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 /**
  * * 区域管理接口实现类
  */
+@RequiredArgsConstructor
 @Service
-public class ISysAreaServiceImpl extends ServiceImpl<SysAreaMapper, SysArea> {
+public class ISysAreaServiceImpl extends ServiceImpl<SysAreaMapper, SysArea> implements ISysAreaService {
 }
